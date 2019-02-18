74c6ee0d69184a349a6cafa7e68a8702dc513a0f
aaazzz <a.matsui0725@gmail.com>
2019-02-18 20:41:06 +0700

    vue-kawaiiにした

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,102 +1,94 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <div class="conaiter">
+      <div class="columns is-centered">
+        <div class="column is-half has-background-light">
+          <h2 class="subtitle">Was this page helpful?</h2>
+          <div class="columns has-text-centered">
+            <div class="column ">
+              <span @click="sendData(3)">
+                <Planet :size="64" mood="blissful" color="#FCCB7E" />
+              </span>
+            </div>
+            <div class="column">
+              <span @click="sendData(2)">
+                <Planet :size="64" mood="happy" color="#FCCB7E" />
+              </span>
+            </div>
+            <div class="column">
+              <span @click="sendData(1)">
+                <Planet :size="64" mood="ko" color="#FCCB7E" />
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { Planet } from "vue-kawaii";
+import axios from "axios";
+import { v1 as uuidv1 } from "uuid";
+import moment from "moment";
+
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  components: {
+    Planet
+  },
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  methods: {
+    toast() {
+      this.$toast.open({
+        duration: 2000,
+        message: `Thanks you for voting!XD`,
+        position: "is-bottom",
+        type: "is-info"
+      });
+    },
+    async sendData(score) {
+      // const apiUrl = 'http://localhost:3000/'
+      const apiUrl =
+        "https://umvu6pfbc5.execute-api.ap-northeast-1.amazonaws.com/dev/";
+      const headers = {
+        "Content-Type": "application/json"
+      };
+      const uuid = uuidv1();
+      await axios
+        .post(
+          apiUrl + "todo",
+          {
+            Item: { id: uuid, score: score }
+          },
+          { headers: headers }
+        )
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+
+      await axios
+        .get(apiUrl + "todo/" + uuid)
+        .then(response => console.table(response.data.Item));
+      console.log("Data sent!");
+      this.toast();
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
--- src/main.js
+++ src/main.js
@@ -2,7 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
 
+library.add(fas)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-layers', FontAwesomeLayers);
+Vue.component('font-awesome-layers-text', FontAwesomeLayersText);
+
+Vue.use(Buefy)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
