be9128d81bcadcc0dbf07c7ac98a45c5dd60af75
aaazzz <a.matsui0725@gmail.com>
2019-02-19 22:42:48 +0700

    状態が保存されるようになった。

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,16 +8,19 @@
             <div class="column">
               <a @click="sendData(3)">
                 <Planet :size="64" mood="blissful" color="#FCCB7E" />
+                <span v-if="vote == 3">Voted!</span>
               </a>
             </div>
             <div class="column">
               <a @click="sendData(2)">
                 <Planet :size="64" mood="happy" color="#FCCB7E" />
+                <span v-if="vote == 2">Voted!</span>
               </a>
             </div>
             <div class="column">
               <a @click="sendData(1)">
                 <Planet :size="64" mood="ko" color="#FCCB7E" />
+                <span v-if="vote == 1">Voted!</span>
               </a>
             </div>
           </div>
@@ -28,67 +31,87 @@
 </template>
 
 <script>
-import { Planet } from "vue-kawaii";
-import axios from "axios";
-import { v1 as uuidv1 } from "uuid";
-import moment from "moment";
+import { Planet } from 'vue-kawaii'
+import axios from 'axios'
+import { v1 as uuidv1 } from 'uuid'
+import moment,{ locale } from 'moment'
+
+const apiUrl = 'http://localhost:3000/'
+// const apiUrl =
+//   "https://umvu6pfbc5.execute-api.ap-northeast-1.amazonaws.com/dev/";
 
 export default {
-  name: "HelloWorld",
+  name: 'HelloWorld',
   components: {
     Planet
   },
   data() {
     return {
-      isFullPage: false
-    };
+      isFullPage: false,
+      vote: 0
+    }
+  },
+  created() {
+    this.setUuid() 
+    this.setVote()
   },
   methods: {
+    setVote() {
+      this.vote = localStorage.getItem(location.pathname) || 0
+    },
+    async setUuid() {
+      if (!localStorage.getItem('uuid')){
+        localStorage.setItem('uuid', uuidv1())
+        localStorage.setItem(location.pathname, 0)
+      }
+    },
     toast() {
       this.$toast.open({
         duration: 1000,
         message: `Thanks you for voting!XD`,
-        position: "is-bottom",
-        type: "is-info"
-      });
+        position: 'is-bottom',
+        type: 'is-info'
+      })
     },
     async sendData(score) {
+      const uuid = localStorage.getItem('uuid')
+
       // Open Loader
       const loadingComponent = this.$loading.open({
         container: this.isFullPage ? null : this.$refs.element.$el
-      });
-      // const apiUrl = 'http://localhost:3000/'
-      const apiUrl =
-        "https://umvu6pfbc5.execute-api.ap-northeast-1.amazonaws.com/dev/";
+      })
       const headers = {
-        "Content-Type": "application/json"
-      };
-      const uuid = uuidv1();
+        'Content-Type': 'application/json'
+      }
+
       await axios
         .post(
-          apiUrl + "todo",
+          apiUrl + 'todo',
           {
             Item: { id: uuid, score: score }
           },
           { headers: headers }
         )
         .then(function(response) {
-          console.log(response);
+          console.log(response)
         })
         .catch(function(error) {
-          console.log(error);
-        });
+          console.log(error)
+        })
 
       await axios
-        .get(apiUrl + "todo/" + uuid)
-        .then(response => console.table(response.data.Item));
-      console.log("Data sent!");
+        .get(apiUrl + 'todo/' + uuid)
+        .then(response => console.table(response.data.Item))
+      console.log('Data sent!')
       // Close Loader
-      loadingComponent.close();
-      this.toast();
+      loadingComponent.close()
+      
+      localStorage.setItem(location.pathname, score)
+      this.setVote()
+      this.toast()
     }
   }
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.js
+++ src/main.js
@@ -6,12 +6,16 @@ import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
+import {
+  FontAwesomeIcon,
+  FontAwesomeLayers,
+  FontAwesomeLayersText
+} from '@fortawesome/vue-fontawesome'
 
 library.add(fas)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-Vue.component('font-awesome-layers', FontAwesomeLayers);
-Vue.component('font-awesome-layers-text', FontAwesomeLayersText);
+Vue.component('font-awesome-layers', FontAwesomeLayers)
+Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
 
 Vue.use(Buefy)
 Vue.config.productionTip = false
